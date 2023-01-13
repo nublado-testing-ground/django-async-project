8b413104080a8a27ec969c7072dc2533ff3675d1
lluvioso <christopher.lluvioso@gmail.com>
2023-01-13 08:52:54 -0800

    ...

--- django-async/django_telegram/views.py
+++ django-async/django_telegram/views.py
@@ -47,10 +47,10 @@ class BotWebhookView(View):
 
                 async with bot.application:
                     await bot.application.start()
-                    await bot.application.process_update(update)
+                    # await bot.application.process_update(update)
+                    await bot.application.update_queue.put(update)
                     await bot.application.stop()
                 logger.info(f"Update from webhook: {update}")
-                #await bot.application.update_queue.put(update)
             except Exception as e:
                 logger.error(f"Error in processing update: {e}")
             return JsonResponse({})
