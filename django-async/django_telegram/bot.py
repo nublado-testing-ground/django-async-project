8ca7f8afdd1749d6619f22a8b94e623bb76a133f
lluvioso <christopher.lluvioso@gmail.com>
2023-01-13 06:02:51 -0800

    ...

--- django-async/django_telegram/bot.py
+++ django-async/django_telegram/bot.py
@@ -34,6 +34,7 @@ class Bot(object):
         )
         self.application = None
         self.webhook_url = webhook_url
+        self.webhook_set = False
 
         try:
             dt = settings.DJANGO_TELEGRAM
@@ -56,15 +57,17 @@ class Bot(object):
         self.application.run_polling()
 
     async def start_webhook(self, webhook_url: str = None):
-        if webhook_url:
-            self.webhook_url = webhook_url
-        if self.webhook_url:
-            await self.telegram_bot.set_webhook(self.webhook_url)
-            logger.info(f"Bot {self.name} webhook set.")
-        else:
-            error = f"Bot {self.token} webhook url isn't set."
-            logger.error(error)
-            raise ImproperlyConfigured(error)
+        if not self.webhook_set:
+            if webhook_url:
+                self.webhook_url = webhook_url
+            if self.webhook_url:
+                await self.telegram_bot.set_webhook(self.webhook_url)
+                self.webhook_set = True
+                logger.info(f"Bot {self.name} webhook set.")
+            else:
+                error = f"Bot {self.token} webhook url isn't set."
+                logger.error(error)
+                raise ImproperlyConfigured(error)
 
     def add_handler(self, handler, handler_group: int = 0):
         try:
